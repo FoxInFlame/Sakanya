8573ba4564cf11cf16a4c41d976ae19d14ceaaa2
FoxInFlame <burningfoxinflame@gmail.com>
2017-08-21 18:17:07 +0200

    Properly use self.bot instead of bot

--- commands/switchversion.py
+++ commands/switchversion.py
@@ -28,25 +28,25 @@ class SwitchVersion():
         await self.bot.add_reaction(context.message, '❎') # Add x mark
         return
       await self.bot.add_reaction(context.message, '✅') # Add checkmark
-      await bot.change_presence(game=discord.Game(name='Updating: Phase 1...', type=0), status=None, afk=False)
+      await self.bot.change_presence(game=discord.Game(name='Updating: Phase 1...', type=0), status=None, afk=False)
       print('Switching Versions...')
       try:
         gitpull = subprocess.run('git pull origin master', check=True, stdout=PIPE)
-        await bot.change_presence(game=discord.Game(name='Updating: Phase 2...', type=0), status=None, afk=False)
+        await self.bot.change_presence(game=discord.Game(name='Updating: Phase 2...', type=0), status=None, afk=False)
         # Git pull successful.
         try:
           switchtag = subprocess.run('git checkout ' + tag, check=True, stdout=PIPE)
-          await bot.change_presence(game=discord.Game(name='Updating: Restarting...', type=0), status=None, afk=False)
+          await self.bot.change_presence(game=discord.Game(name='Updating: Restarting...', type=0), status=None, afk=False)
           # Checkout succeeded
           await self.bot.logout()
           os.execl(sys.executable, sys.executable, *sys.argv)
         except subprocess.CalledProcessError as e:
           await self.bot.say('Saknya ran into an error while switching tags.\n````' + e.output + '```')
-        await bot.change_presence(game=discord.Game(name='Updating: Error in phase 2...', type=0), status=None, afk=False)
+        await self.bot.change_presence(game=discord.Game(name='Updating: Error in phase 2...', type=0), status=None, afk=False)
         # It will change to an emoji automatically in less than 15 minutes, so leave the status like that.
       except subprocess.CalledProcessError as e:
         await self.bot.say('Sakanya ran into an error while pulling from git.\n```' + e.output + '```')
-        await bot.change_presence(game=discord.Game(name='Updating: Error in phase 1...', type=0), status=None, afk=False)
+        await self.bot.change_presence(game=discord.Game(name='Updating: Error in phase 1...', type=0), status=None, afk=False)
 
 def setup(bot):
   bot.add_cog(SwitchVersion(bot))
