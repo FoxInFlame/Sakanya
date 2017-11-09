85c6aec732ed6cbdf34622b73ee44479a021cdb2
FoxInFlame <burningfoxinflame@gmail.com>
2017-11-10 01:38:45 +0900

    Try something out.

--- other/botchains.py
+++ other/botchains.py
@@ -8,6 +8,7 @@ from __main__ import SakanyaCore
 class BotChains():
   def __init__(self, bot):
     self.bot = bot
+    self.times = 5
 
   @commands.command(pass_context=True)
   async def hello(self, context):
@@ -21,6 +22,18 @@ class BotChains():
       >hello
     """
     await self.bot.say('n:hello')
+
+  @commands.command(pass_context=True)
+  async def helloreset(self, context):
+    if context.message.author.id == '202501452596379648':
+      self.times = 5
+
+  async def on_message(self, message):
+    if message.author.id == '270198146020278272' and message.content == 'k/hello':
+      self.times = self.times - 1
+      if self.times >= 0:
+        await self.bot.send_message(message.channel, 'n:hello')
+
 
 def setup(bot):
   bot.add_cog(BotChains(bot))
