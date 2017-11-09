4582f69ad72529e855b204a775670560b292c783
FoxInFlame <burningfoxinflame@gmail.com>
2017-11-10 01:48:17 +0900

    Was all a joke... sry

--- other/botchains.py
+++ other/botchains.py
@@ -8,7 +8,7 @@ from __main__ import SakanyaCore
 class BotChains():
   def __init__(self, bot):
     self.bot = bot
-    self.times = 5
+    #self.times = 3
 
   @commands.command(pass_context=True, aliases=['poke', 'hello'])
   async def chain(self, context):
@@ -23,16 +23,16 @@ class BotChains():
     """
     await self.bot.say('n:hello')
 
-  @commands.command(pass_context=True)
-  async def helloreset(self, context):
-    if context.message.author.id == '202501452596379648':
-      self.times = 3
+  #@commands.command(pass_context=True)
+  #async def helloreset(self, context):
+  #  if context.message.author.id == '202501452596379648':
+  #    self.times = 3
 
-  async def on_message(self, message):
-    if message.author.id == '270198146020278272' and message.content == 'k/hello':
-      self.times = self.times - 1
-      if self.times >= 0:
-        await self.bot.send_message(message.channel, 'n:hello')
+  #async def on_message(self, message):
+  #  if message.author.id == '270198146020278272' and message.content == 'k/hello':
+  #    self.times = self.times - 1
+  #    if self.times >= 0:
+  #      await self.bot.send_message(message.channel, 'n:hello')
 
 
 def setup(bot):
