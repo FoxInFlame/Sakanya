b975750732b3ed9107219acbd745ad17fc0f5af9
FoxInFlame <burningfoxinflame@gmail.com>
2018-01-03 22:10:04 +0900

    Fix invalid syntax

--- other/presenceupdate.py
+++ other/presenceupdate.py
@@ -69,6 +69,8 @@ class PresenceUpdate():
         kaomoji = random.choice(self.custom_presences)
 
       type = random.randint(0, 3)
+      if type == 3:
+        kaomoji = 'you'
       await self.bot.change_presence(game=discord.Game(name=kaomoji, type=type), status=None, afk=False)
       await asyncio.sleep(SakanyaCore().presenceupdate_timer)
 
