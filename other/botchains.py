27a124b21e07e5ff16c40e782980ddff6fbfcb65
FoxInFlame <burningfoxinflame@gmail.com>
2018-01-23 22:03:37 +0900

    Add support for b/character

--- other/botchains.py
+++ other/botchains.py
@@ -14,20 +14,29 @@ class BotChains():
   
   async def on_message(self, message):
     """
-    Handle MAL Anime links for Andre
+    Handle MAL links for Andre and BobDono
     """
     if message.server is not None and message.server.id == '317924870950223872':
       try:
+        # Anime Links
         resultanime = re.search(r'https://myanimelist.net/anime/([0-9]+?)/', message.content).group(1)
         if resultanime is not None:
           await self.bot.send_message(message.channel, '!anime ' + resultanime)
       except:
         pass
       try:
-        # Somehow the following didn't work...?
+        # Manga Links
         resultmanga = re.search(r'https://myanimelist.net/manga/([0-9]+?)/', message.content).group(1)
         if resultmanga is not None:
           await self.bot.send_message(message.channel, '!manga ' + resultmanga)
+      except:
+        pass
+      try:
+        # Character Links
+        resultcharacter = re.search(
+            r'https://myanimelist.net/character/([0-9]+?)/', message.content).group(1)
+        if resultcharacter is not None:
+          await self.bot.send_message(message.channel, 'b/character ' + resultcharacter)
       except:
         pass
         
