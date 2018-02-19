fa458a7d6d670bfee9ee977e6d3ad0a1172b8940
FoxInFlame <burningfoxinflame@gmail.com>
2018-02-19 23:21:07 +0900

    Remove unused code.

--- other/botchains.py
+++ other/botchains.py
@@ -53,17 +53,6 @@ class BotChains():
     """
     await self.bot.say('n:hello')
 
-  #@commands.command(pass_context=True)
-  #async def helloreset(self, context):
-  #  if context.message.author.id == '202501452596379648':
-  #    self.times = 3
-
-  #async def on_message(self, message):
-  #  if message.author.id == '270198146020278272' and message.content == 'k/hello':
-  #    self.times = self.times - 1
-  #    if self.times >= 0:
-  #      await self.bot.send_message(message.channel, 'n:hello')
-
 
 def setup(bot):
   bot.add_cog(BotChains(bot))
